960d9b68c0d26f5273214f5d9920ae22d7bfd353
Muhammad Usama <usama6000@gmail.com>
2020-03-10 12:37:56 +0500

    Error handling if user not found

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -3,15 +3,17 @@ const ErrorResponse = require('../../utils/errorResponse');
 const User = require('../models/User');
 
 exports.me = asyncHandler(async (req, res, next) => {
-  return res.json({success: true, data: req.user});
+  return res.json({ success: true, data: req.user });
 })
 
 exports.getById = asyncHandler(async (req, res, next) => {
   const { id } = req.params;
   const user = await User.findById(id);
-  if(!user) {
-    return new ErrorResponse('User not found', 400);
+  if (!user) {
+    return next(
+      new ErrorResponse('User not found', 400)
+    )
   }
 
-  return res.json({success: true, data: user})
+  return res.json({ success: true, data: user })
 })
